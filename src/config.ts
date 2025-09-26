30fc8d7c9bd22fd0aef9d722a569dc7494019ea8
meteorain <liuweinan85@gmail.com>
2025-09-26 15:46:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "刘疏影",
-	bio: "Seed of Hope",
+	bio: "Shadow of Hope",
 	links: [
 		{
 			name: "Twitter",
