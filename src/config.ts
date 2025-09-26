25b572658fcaabb59e869cec175ca9a50b3dc560
meteorain <liuweinan85@gmail.com>
2025-09-26 15:44:55 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "疏影",
-	subtitle: "Seed of Hope",
+	title: "刘疏影",
+	subtitle: "Shadow of Hope",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
